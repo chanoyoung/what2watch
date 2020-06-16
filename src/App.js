5af532bac4d6921011843c6de64e621d5a86188a
YoungChance <diruo0725@gmail.com>
2020-06-16 22:04:44 +0900

    version 1.0
    api로 데이터 받아와서 화면에 표시. css 적용 X

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Movie from './Movie';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sorty_by=rating")
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+    // this.setState({ isLoading: false });
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">"Loading..."</span>
+          </div>
+        ) : (
+          <div className="movies">
+              {movies.map(movie => (
+                <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}
+                />
+            ))}
+          </div>
+      )}
+      </section>
+      );
+  }
 }
+
 
 export default App;
