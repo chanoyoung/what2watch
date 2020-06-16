f20bfefad64b7e8c5ab65028d32028763041c49b
YoungChance <diruo0725@gmail.com>
2020-06-16 22:21:36 +0900

    Updates

---
+++ precache-manifest.ebc7e984c11f34cbf962f737162256ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f995416e0c26be43e87ca0ecd0e1d28a",
+    "url": "/what2watch/index.html"
+  },
+  {
+    "revision": "4f1485861a9548c5bcc5",
+    "url": "/what2watch/static/css/main.5cad8f57.chunk.css"
+  },
+  {
+    "revision": "a2344de6c1d8226f15b7",
+    "url": "/what2watch/static/js/2.9a639023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/what2watch/static/js/2.9a639023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1485861a9548c5bcc5",
+    "url": "/what2watch/static/js/main.601f0bdf.chunk.js"
+  },
+  {
+    "revision": "72633632ee89d306c51e",
+    "url": "/what2watch/static/js/runtime-main.0c086b11.js"
+  }
+]);
