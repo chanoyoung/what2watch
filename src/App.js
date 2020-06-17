3f4a162a4184d2a029f6c5103cc34acd5193daff
YoungChance <diruo0725@gmail.com>
2020-06-17 20:52:39 +0900

    added router
    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  }
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sorty_by=rating")
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-    // this.setState({ isLoading: false });
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">"Loading..."</span>
-          </div>
-        ) : (
-          <div className="movies">
-              {movies.map(movie => (
-                <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}
-                />
-            ))}
-          </div>
-      )}
-      </section>
-      );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+    </HashRouter>
+  );
 }
-
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ function Movie({id, year, title, summary, poster, genres}) {
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
+                <ul className="movie__genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
                 <p className="movie__summary">{summary.slice(0, 180)}{summary.length>180?'...':''}</p>
             </div>
         </div>
